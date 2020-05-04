aedf8f97065ead012a529b1923afa0b75ffd235b
Kortina <kortina@gmail.com>
2020-05-04 08:59:32 -0700

    fix new post

--- new-post.sh
+++ new-post.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
 fi
 
 title="$1"
-t="`echo "$title" | iconv -t ascii//TRANSLIT | sed -E 's/[~\^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+\|-+$//g' | tr A-Z a-z | sed 's/[- \n]*$//g'`"
+t="`echo "$title" | iconv -t ascii//TRANSLIT | sed -E 's/[~\^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+\|-+$//g' | tr A-Z a-z | sed 's/[- \r]*$//g'`"
 d="`date +'%Y-%m-%d'`"
 f="_posts/$d-$t.md"
 
